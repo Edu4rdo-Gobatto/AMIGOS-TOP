2748b3538928007b4a1942efe76eb129704ee7eb
EDUARDO GOBATTO <eduardogobt@gmail.com>
2025-07-24 14:10:58 -0400

    Ajustes finais, font - size / font-theme

--- src/pages/quemSomos/quemSomos.tsx
+++ src/pages/quemSomos/quemSomos.tsx
@@ -8,23 +8,25 @@ function QuemSomos (){
             <div className={styles.text}>
                 <h1>QUEM <br />SOMOS</h1><br />
                 <h4>Movidos por tecnologia: Sempre com um olhar para o futuro!<br /><br />
-                    Em meados do ano 2000, foi criado pelo empresário Adenilson Rocha o TopSinop, pimeiro site de entretenimento
+                    Em meados do ano 2000, foi criado pelo empresário Adenilson Rocha o TopSinop, pimeiro site de entretenimento 
                     do norte de Mato Grosso. A visão empreendedora de Adenilson mostrava um caminho de sucesso e logo em 2002,
                     Adenilson criou a MultiWare Tecnologia, com sede em Sinop/MT, a empresa inicialmente era voltada em assistência
                     técnica de computadores com atendimento domiciliar. O empreendedor enxergou uma lacuna a ser no mercado,
                     então surgiu a ideia de criar um sistema de gestão para empresas de telecomunicação, sendo atualmente 
                     umas das principais marcas deste segmento nacional, o TopSapp.
                     <p>
-                        A partir do TopSapp a Multiware Tecnologia cresceu rapidamente e surgem outras marcas como TwSpeed Telecom, A Folha Notícias, 
-                        TopDNS e TopCabo. Em 2019 um novo capítulo na história passa a ser escrito, surge o Grupo Ad Brax.
+                        <h4>A partir do TopSapp a Multiware Tecnologia cresceu rapidamente e surgem outras marcas como TwSpeed Telecom, A Folha Notícias, 
+                        TopDNS e TopCabo. Em 2019 um novo capítulo na história passa a ser escrito, surge o Grupo Ad Brax.</h4>
                     </p>
                     <p>
-                        Ao longo de sua existência, Grupo Ad Brax ampliou seu rol de serviços para a área de Tecnologia da Informação. Assim, nós, do
-                        Grupo Ad Brax, desenvolvemos o nosso jeito de ser movidos por tecnologia, investindo em empresas que vão ao encontro das
-                        necessidades de empresas e da sociedade.
+                        <h4>
+                            Ao longo de sua existência, Grupo Ad Brax ampliou seu rol de serviços para a área de Tecnologia da Informação. Assim, nós, do
+                            Grupo Ad Brax, desenvolvemos o nosso jeito de ser movidos por tecnologia, investindo em empresas que vão ao encontro das
+                            necessidades de empresas e da sociedade.
+                        </h4>
                     </p>
                     <p>
-                        O grupo Ad Brax é um sonho que se tornou realidade.
+                        <h4>O grupo Ad Brax é um sonho que se tornou realidade.</h4>
                     </p>
                 </h4>
                 
